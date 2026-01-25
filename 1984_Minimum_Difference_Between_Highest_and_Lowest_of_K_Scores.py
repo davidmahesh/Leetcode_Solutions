fbe7c7093e515cc677574d1a6b76f1375546bdaa
Y David Mahesh <147488196+davidmahesh@users.noreply.github.com>
2026-01-25 22:07:20 +0530

    Add files via upload

---
+++ 1984_Minimum_Difference_Between_Highest_and_Lowest_of_K_Scores.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minimumDifference(self, nums, k):
+        if k <= 1:
+            return 0
+
+        nums.sort()
+        ans = float('inf')
+
+        for i in range(len(nums) - k + 1):
+            diff = nums[i + k - 1] - nums[i]
+            if diff < ans:
+                ans = diff
+
+        return ans
