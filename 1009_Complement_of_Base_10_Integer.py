0f8df1614e3bbb06a898cec2045c71271dd54429
Y David Mahesh <147488196+davidmahesh@users.noreply.github.com>
2026-03-11 22:07:36 +0530

    Add files via upload

---
+++ 1009_Complement_of_Base_10_Integer.py
@@ -0,0 +1,7 @@
+class Solution:
+    def bitwiseComplement(self, n):
+        if n == 0:
+            return 1
+        bits = n.bit_length()
+        mask = (1 << bits) - 1
+        return n ^ mask
