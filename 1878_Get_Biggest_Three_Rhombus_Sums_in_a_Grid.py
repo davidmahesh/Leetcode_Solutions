148a8d59bdade6020303afa68c9d4360655ca9bc
Y David Mahesh <147488196+davidmahesh@users.noreply.github.com>
2026-03-16 20:38:31 +0530

    Add files via upload

---
+++ 1878_Get_Biggest_Three_Rhombus_Sums_in_a_Grid.py
@@ -0,0 +1,25 @@
+class Solution:
+    def getBiggestThree(self, grid):
+        m, n = len(grid), len(grid[0])
+        sums = set()
+
+        for r in range(m):
+            for c in range(n):
+                sums.add(grid[r][c])
+                for size in range(1, min(m, n)):
+                    if r - size < 0 or r + size >= m:
+                        break
+                    if c - size < 0 or c + size >= n:
+                        break
+
+                    total = 0
+                    for d in range(size):
+                        total += grid[r - size + d][c + d]
+                        total += grid[r + d][c + size - d]
+                        total += grid[r + size - d][c - d]
+                        total += grid[r - d][c - size + d]
+
+                    sums.add(total)
+
+        top = sorted(sums, reverse=True)[:3]
+        return top
