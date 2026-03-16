78309039319a76243f12b727205c107571811ca9
Y David Mahesh <147488196+davidmahesh@users.noreply.github.com>
2026-03-16 20:41:56 +0530

    Refactor getBiggestThree method for clarity

--- 1878_Get_Biggest_Three_Rhombus_Sums_in_a_Grid.py
+++ 1878_Get_Biggest_Three_Rhombus_Sums_in_a_Grid.py
@@ -2,7 +2,6 @@ class Solution:
     def getBiggestThree(self, grid):
         m, n = len(grid), len(grid[0])
         sums = set()
-
         for r in range(m):
             for c in range(n):
                 sums.add(grid[r][c])
@@ -11,15 +10,12 @@ class Solution:
                         break
                     if c - size < 0 or c + size >= n:
                         break
-
                     total = 0
                     for d in range(size):
                         total += grid[r - size + d][c + d]
                         total += grid[r + d][c + size - d]
                         total += grid[r + size - d][c - d]
                         total += grid[r - d][c - size + d]
-
                     sums.add(total)
-
         top = sorted(sums, reverse=True)[:3]
         return top
