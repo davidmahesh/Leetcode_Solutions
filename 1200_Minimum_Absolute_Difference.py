6142e1eacf808f455e14d3d08f6dbd2210d10481
Y David Mahesh <147488196+davidmahesh@users.noreply.github.com>
2026-04-03 23:08:43 +0530

    Update 1200_Minimum_Absolute_Difference.py

--- 1200_Minimum_Absolute_Difference.py
+++ 1200_Minimum_Absolute_Difference.py
@@ -3,7 +3,6 @@ class Solution:
         arr.sort()
         res = []
         min_diff = float('inf')
-
         for i in range(1, len(arr)):
             diff = arr[i] - arr[i - 1]
             if diff < min_diff:
@@ -11,5 +10,4 @@ class Solution:
                 res = [[arr[i - 1], arr[i]]]
             elif diff == min_diff:
                 res.append([arr[i - 1], arr[i]])
-
         return res
