e571d8053e8789c765798f1dff34d6cd5f3910ff
Y David Mahesh <147488196+davidmahesh@users.noreply.github.com>
2026-04-24 22:21:49 +0530

    Add files via upload

---
+++ 2833_Furthest_Point_From_Origin.py
@@ -0,0 +1,6 @@
+class Solution:
+    def furthestDistanceFromOrigin(self, moves):
+        L = moves.count('L')
+        R = moves.count('R')
+        blanks = moves.count('_')
+        return abs(L - R) + blanks
