a91ad05736d7ff258af8f9a2f9265a2784bab479
Y David Mahesh <147488196+davidmahesh@users.noreply.github.com>
2026-05-21 22:03:05 +0530

    Add files via upload

---
+++ 3043_Find_the_Length_of_the_Longest_Common_Prefix.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestCommonPrefix(self, arr1, arr2):
+        seen = set()
+        for x in arr1:
+            s = str(x)
+            for i in range(1, len(s)+1):
+                seen.add(s[:i])
+        res = 0
+        for y in arr2:
+            s = str(y)
+            for i in range(1, len(s)+1):
+                if s[:i] in seen:
+                    res = max(res, i)
+        return res
