70e5669a56aa84e85997b14faa72e247cc0c0ee3
Y David Mahesh <147488196+davidmahesh@users.noreply.github.com>
2026-06-25 23:18:48 +0530

    Add files via upload

---
+++ 3737_Count_Subarrays_With_Majority_Element_I.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countMajoritySubarrays(self,nums,target):
+        n=len(nums)
+        ans=0
+        for i in range(n):
+            cnt=0
+            total=0
+            for j in range(i,n):
+                total+=1
+                if nums[j]==target:
+                    cnt+=1
+                if cnt*2>total:
+                    ans+=1
+        return ans
