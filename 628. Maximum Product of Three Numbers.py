24170d72fc53ca306e2179aafb6f6ee66ec4d36a
David Mahesh Yarlagadda <147488196+davidmahesh@users.noreply.github.com>
2026-07-26 22:46:47 +0530

    Add files via upload

---
+++ 628. Maximum Product of Three Numbers.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums)
+        return max(nums[0] * nums[1] * nums[n - 1],
+                   nums[n - 1] * nums[n - 2] * nums[n - 3])
