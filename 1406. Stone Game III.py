8b352549a382ae2c524977109f0dcfa89488ef95
David Mahesh Yarlagadda <147488196+davidmahesh@users.noreply.github.com>
2026-08-03 21:41:32 +0530

    Add files via upload

---
+++ 1406. Stone Game III.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def stoneGameIII(self, stoneValue: List[int]) -> str:
+        n = len(stoneValue)
+        dp = [0] * (n + 1)
+
+        for i in range(n - 1, -1, -1):
+            s = 0
+            dp[i] = -10 ** 18
+            for j in range(i, min(i + 3, n)):
+                s += stoneValue[j]
+                dp[i] = max(dp[i], s - dp[j + 1])
+
+        if dp[0] > 0:
+            return "Alice"
+        if dp[0] < 0:
+            return "Bob"
+        return "Tie"
