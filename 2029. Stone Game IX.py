f4dde5861a86512cbe12d0838cd299704b2a6a93
David Mahesh Yarlagadda <147488196+davidmahesh@users.noreply.github.com>
2026-08-16 23:02:03 +0530

    Add files via upload

---
+++ 2029. Stone Game IX.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def stoneGameIX(self, stones: List[int]) -> bool:
+        cnt = [0, 0, 0]
+        for x in stones:
+            cnt[x % 3] += 1
+        if cnt[0] % 2 == 0:
+            return cnt[1] >= 1 and cnt[2] >= 1
+        return abs(cnt[1] - cnt[2]) > 2
