73565a039391a47031625589c9b9cc69433fec0e
Mahesh_369 <147488196+davidmahesh@users.noreply.github.com>
2026-08-31 22:55:02 +0530

    Update 2058. Find the Minimum and Maximum Number of Nodes Between Critical Points.py

--- 2058. Find the Minimum and Maximum Number of Nodes Between Critical Points.py
+++ 2058. Find the Minimum and Maximum Number of Nodes Between Critical Points.py
@@ -6,11 +6,9 @@ class Solution:
         first = -1
         last = -1
         min_dist = float('inf')
-
         while curr and curr.next:
             if (curr.val > prev.val and curr.val > curr.next.val) or \
                (curr.val < prev.val and curr.val < curr.next.val):
-
                 if first == -1:
                     first = pos
                 else:
