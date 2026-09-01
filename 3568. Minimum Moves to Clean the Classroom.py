392385389550cf95c4cd4ce5471eaa1a1848ea3b
Mahesh_369 <147488196+davidmahesh@users.noreply.github.com>
2026-09-01 23:35:55 +0530

    Add files via upload

---
+++ 3568. Minimum Moves to Clean the Classroom.py
@@ -0,0 +1,71 @@
+from collections import deque
+
+class Solution:
+    def minMoves(self, classroom, energy):
+        m = len(classroom)
+        n = len(classroom[0])
+
+        start = None
+        litter = {}
+
+        for i in range(m):
+            for j in range(n):
+                if classroom[i][j] == 'S':
+                    start = (i, j)
+                elif classroom[i][j] == 'L':
+                    litter[(i, j)] = len(litter)
+
+        k = len(litter)
+
+        if k == 0:
+            return 0
+
+        full = (1 << k) - 1
+        sr, sc = start
+
+        best = {}
+        q = deque()
+
+        mask = 0
+        if (sr, sc) in litter:
+            mask |= 1 << litter[(sr, sc)]
+
+        best[(sr, sc, mask)] = energy
+        q.append((sr, sc, energy, mask, 0))
+
+        while q:
+            x, y, e, mask, dist = q.popleft()
+
+            if mask == full:
+                return dist
+
+            for dx, dy in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+                nx = x + dx
+                ny = y + dy
+
+                if nx < 0 or nx >= m or ny < 0 or ny >= n:
+                    continue
+
+                if classroom[nx][ny] == 'X':
+                    continue
+
+                if e == 0:
+                    continue
+
+                ne = e - 1
+
+                if classroom[nx][ny] == 'R':
+                    ne = energy
+
+                nm = mask
+
+                if (nx, ny) in litter:
+                    nm |= 1 << litter[(nx, ny)]
+
+                state = (nx, ny, nm)
+
+                if ne > best.get(state, -1):
+                    best[state] = ne
+                    q.append((nx, ny, ne, nm, dist + 1))
+
+        return -1
