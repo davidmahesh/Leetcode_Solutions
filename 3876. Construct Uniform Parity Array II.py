2e9ab8051db0822552a31b5bdebd94b9be43a820
Mahesh_369 <147488196+davidmahesh@users.noreply.github.com>
2026-09-03 21:27:01 +0530

    Add files via upload

---
+++ 3876. Construct Uniform Parity Array II.py
@@ -0,0 +1,20 @@
+class Solution:
+    def uniformArray(self, nums1):
+        nums1.sort()
+        possible = {nums1[0] % 2}
+        seen = [False, False]
+        seen[nums1[0] % 2] = True
+
+        for x in nums1[1:]:
+            options = {x % 2}
+            for p in range(2):
+                if seen[p]:
+                    options.add((x - p) % 2)
+
+            possible &= options
+            if not possible:
+                return False
+
+            seen[x % 2] = True
+
+        return True
